ea576682892dec06049afe24327f7ea4fb682b07
alexander <rud.sasha1997@gmail.com>
2018-11-08 20:37:30 +0300

    upd

--- TestASTParser/Tests.cs
+++ TestASTParser/Tests.cs
@@ -56,9 +56,11 @@ namespace TestASTParser
         [Test]
         public void TestRepeat()
         {
-            var tree = ASTParserTests.Parse("begin repeat a:=2 until 2 end");
-            Assert.AreEqual("ProgramTree.RepeatNode, SimpleLang", (string)tree["StList"]["$values"][0]["$type"]);
-            // TODO: проверить узлы содержимого repeat
+            var tree = ASTParserTests.Parse("begin repeat a:=10 until 8 end");
+            Assert.AreEqual("ProgramTree.RepeatNode, SimpleLang", (string)tree["StList"]["$values"][0]["$type"]);   
+            Assert.AreEqual("ProgramTree.IntNumNode, SimpleLang", (string)tree["StList"]["$values"][0]["Expr"]["$type"]);
+            Assert.AreEqual("8", ((string)tree["StList"]["$values"][0]["Expr"]["Num"]).Trim());
+            Assert.AreEqual("ProgramTree.BlockNode, SimpleLang", (string)tree["StList"]["$values"][0]["Stat"]["$type"]);
         }
     }
     
@@ -71,7 +73,9 @@ namespace TestASTParser
         {
             var tree = ASTParserTests.Parse("begin for i:=2 to 10 do a:=2 end");
             Assert.AreEqual("ProgramTree.ForNode, SimpleLang", (string)tree["StList"]["$values"][0]["$type"]);
-            // TODO: проверить узлы содержимого for
+            Assert.AreEqual("ProgramTree.AssignNode, SimpleLang", (string)tree["StList"]["$values"][0]["Ass"]["$type"]);
+            Assert.AreEqual("2", ((string)tree["StList"]["$values"][0]["Ass"]["Expr"]["Num"]).Trim());
+            Assert.AreEqual("ProgramTree.AssignNode, SimpleLang", (string)tree["StList"]["$values"][0]["Stat"]["$type"]);
         }
     }
     
@@ -84,7 +88,48 @@ namespace TestASTParser
         {
             var tree = ASTParserTests.Parse("begin write(2) end");
             Assert.AreEqual("ProgramTree.WriteNode, SimpleLang", (string)tree["StList"]["$values"][0]["$type"]);
-            // TODO: проверить содержимое write
+            Assert.AreEqual("ProgramTree.IntNumNode, SimpleLang", (string)tree["StList"]["$values"][0]["Expr"]["$type"]);
+            Assert.AreEqual("2", ((string)tree["StList"]["$values"][0]["Expr"]["Num"]).Trim());
+        }
+    }
+
+    [TestFixture]
+    public class IfTests
+    {
+
+        [Test]
+        public void TestIf()
+        {
+            var tree = ASTParserTests.Parse("begin if 3 then a:= 5 end");
+            Assert.AreEqual("ProgramTree.IfNode, SimpleLang", (string)tree["StList"]["$values"][0]["$type"]);
+            Assert.AreEqual("ProgramTree.IntNumNode, SimpleLang", (string)tree["StList"]["$values"][0]["Expr"]["$type"]);
+            Assert.AreEqual("3", ((string)tree["StList"]["$values"][0]["Expr"]["Num"]).Trim());
+            Assert.AreEqual("ProgramTree.AssignNode, SimpleLang", (string)tree["StList"]["$values"][0]["StTrue"]["$type"]);
+        }
+
+        [Test]
+        public void TestIfElse()
+        {
+            var tree = ASTParserTests.Parse("begin if 7 then a:= 5 else a:= 9 end");
+            Assert.AreEqual("ProgramTree.IfNode, SimpleLang", (string)tree["StList"]["$values"][0]["$type"]);
+            Assert.AreEqual("ProgramTree.IntNumNode, SimpleLang", (string)tree["StList"]["$values"][0]["Expr"]["$type"]);
+            Assert.AreEqual("7", ((string)tree["StList"]["$values"][0]["Expr"]["Num"]).Trim());
+            Assert.AreEqual("ProgramTree.AssignNode, SimpleLang", (string)tree["StList"]["$values"][0]["StFalse"]["$type"]);
+        }
+    }
+
+    [TestFixture]
+    public class BinaryTests
+    {
+
+        [Test]
+        public void TestBinary()
+        {
+            var tree = ASTParserTests.Parse("begin a := (5*2) * (9-1) end");
+            Assert.AreEqual("ProgramTree.BinaryNode, SimpleLang", (string)tree["StList"]["$values"][0]["Expr"]["$type"]);
+            Assert.AreEqual("ProgramTree.BinaryNode, SimpleLang", (string)tree["StList"]["$values"][0]["Expr"]["Left"]["$type"]);
+            Assert.AreEqual("ProgramTree.IntNumNode, SimpleLang", (string)tree["StList"]["$values"][0]["Expr"]["Left"]["Left"]["$type"]);
+            Assert.AreEqual("5", ((string)tree["StList"]["$values"][0]["Expr"]["Left"]["Left"]["Num"]).Trim());
         }
     }
 }
